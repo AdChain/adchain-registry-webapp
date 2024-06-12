a18059ae4ec24a15c65c5aa981d3ec9e7cba58a2
Matt Zamora <mzamora2899@gmail.com>
2024-06-12 16:09:52 -0600

    fix more dep

--- src/components/single_domain/DomainProfileComments.js
+++ src/components/single_domain/DomainProfileComments.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Disqus from 'react-disqus-comments'
+import React from "react";
+import PropTypes from "prop-types";
 
-import './DomainProfileComments.css'
+import "./DomainProfileComments.css";
 
-function DomainProfileComments (props) {
-  const {domain} = props
+function DomainProfileComments(props) {
+  const { domain } = props;
 
-  const comments = (<div className='DomainProfileComments'>
-    <Disqus
+  const comments = (
+    <div className="DomainProfileComments">
+      {/* <Disqus
       shortname='adchain'
       identifier={domain}
       title={domain}
-      url={`https://app.adchain.com/domains/${domain}`} />
-  </div>)
+      url={`https://app.adchain.com/domains/${domain}`} /> */}
+    </div>
+  );
 
-  return (comments)
+  return comments;
 }
 
 DomainProfileComments.propTypes = {
-  domain: PropTypes.string
-}
+  domain: PropTypes.string,
+};
 
-export default DomainProfileComments
+export default DomainProfileComments;
